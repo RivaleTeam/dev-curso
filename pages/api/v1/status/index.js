c297cf78241735366f34ab37a05fad2cee04a284
RivaleTeam <128907877+RivaleTeam@users.noreply.github.com>
2025-07-23 22:32:15 +0000

    `max_connections`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,31 @@
-import database from "../../../../infra/database.js"
+import database from "infra/database.js"
 
 async function status(request, response){
-    const result = await database.query('SELECT 1 + 1');
-    console.log(result)
-    response.status(200).json({chave:'bla bla bla'})
+    const resultVersion = await database.query('SHOW server_version');
+    const maxConnections = await database.query('SHOW max_connections;');
+
+    const databaseName = process.env.POSTGRES_DB;
+
+    const databaseOpenedConnections = await database.query({
+        text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+        values: [databaseName]
+    });
+
+    //const databaseOpenedConnections = await database.query("SELECT count(*)::int FROM pg_stat_activity WHERE datname = 'local';");
+    
+    
+    const version = resultVersion.rows[0].server_version;
+    const maxDb = maxConnections.rows[0].max_connections;
+    const databaseOpened = databaseOpenedConnections.rows[0].count;
+
+    response.status(200).json({
+        api:'bla bla bla',
+        db:{
+            version: version,
+            max_connections: maxDb,
+            open_connection: databaseOpened
+        }
+    })
 }
 
 export default status;
